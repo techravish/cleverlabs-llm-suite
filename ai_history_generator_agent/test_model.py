09f8553ab15f2df8bc0202c8160af13de1ea7999
techravish <techravish@gmail.com>
2026-03-06 22:51:42 +0000

    History - test model delete

--- ai_history_generator_agent/test_model.py
+++
@@ -1,6 +0,0 @@
-# test_model.py
-
-from services.local_llm_service import generate_history_local
-
-story = generate_history_local("A story about a lion, giraffe, and naughty monkey.")
-print(story)
